0cbc58eb9ff2208d9608da144248d998094c49ec
qrlpx <qrlpx@users.noreply.github.com>
2015-08-08 19:19:22 +0200

    doc

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@
 //! * Single alternative options are a _special case_ of options/alternatives. 
 //!   To avoid confusion, all code & doc related to single alternative options 
 //!   should be listed after regular options/alternatives.
+//! * Panics vs error?
+//! * remove_option, remove_alternative?
 
 #![feature(get_type_id)]
 #![feature(box_syntax)]
