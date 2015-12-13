69065f2b72d6ed5db349afae7d378899225d18c3
Syrws <accounts@felix-koepge.de>
2015-12-14 00:12:02 +0100

    blanket-impl for reflect::Key

--- src/reflect.rs
+++ src/reflect.rs
@@ -3,6 +3,10 @@ use std::fmt::Debug;
 
 pub trait Key: Debug + Clone + Ord + Any {}
 
+impl<T> Key for T
+    where T: Debug + Clone + Ord + Any
+{}
+
 pub trait Object: Any + Sized {
     type Key: Key = String;
     fn key() -> &'static Self::Key;
